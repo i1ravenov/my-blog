2d501a92d3e56148735daf08cad2749287a0ee65
Ivan Voronov <i1ravenov@gmail.com>
2026-05-05 18:57:49 +0400

    Added logging with AOP AspectJ

--- my-blog-back-app/src/main/java/ru/yandex/practicum/WebConfiguration.java
+++ my-blog-back-app/src/main/java/ru/yandex/practicum/WebConfiguration.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -9,4 +10,5 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @ComponentScan(basePackages = "ru.yandex.practicum")
 @PropertySource("classpath:application.properties")
+@EnableAspectJAutoProxy
 public class WebConfiguration {}
--- my-blog-back-app/src/main/resources/schema.sql
+++ my-blog-back-app/src/main/resources/schema.sql
@@ -7,7 +7,21 @@ CREATE TABLE IF NOT EXISTS post (
     comments_count INT DEFAULT 0
 );
 
+CREATE TABLE IF NOT EXISTS comment (
+    id BIGSERIAL PRIMARY KEY,
+    text TEXT NOT NULL,
+    post_id BIGINT NOT NULL,
+    CONSTRAINT fk_comment_post
+        FOREIGN KEY (post_id)
+        REFERENCES post(id)
+        ON DELETE CASCADE
+);
+
 INSERT INTO post (id, title, text, tags, likes_count, comments_count) VALUES
 (1, 'First Post', 'This is my first post', '["java","spring"]', 10, 2),
 (2, 'Second Post', 'Learning Spring is fun', '["spring","backend"]', 25, 5),
 (3, 'Third Post', 'Microservices guide', '["microservices","architecture"]', 40, 8);
+
+INSERT INTO comment (id, text, post_id) VALUES
+(1, 'Комментарий к посту 1', 1),
+(2, 'Ещё один комментарий к посту 1', 1);
